0fa4569e9dc6595bed240cf4436c71bf7d9844f5
mikhail.ostanin@signify.com <mikhail.ostanin@gmail.com>
2026-02-15 20:34:17 +0100

    fixtures updated with db population

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
+import json
+
 import pytest
+from sqlalchemy import text
 
 from httpx import ASGITransport, AsyncClient
 from src.main import app
@@ -10,7 +13,6 @@ from src.models import *
 
 @pytest.fixture(scope="session", autouse=True)
 async def async_main():
-
     assert settings.MODE == "TEST"
 
     async with engine_null_pool.begin() as conn:
@@ -30,3 +32,16 @@ async def sign_up(async_main):
                 "password": "test_pwd"
             }
         )
+
+@pytest.fixture(scope="session", autouse=True)
+async def populate_db(sign_up):
+    with open("tests/mock_hotels.json") as file:
+        hotels = json.load(file)
+    with open("tests/mock_rooms.json") as file:
+        rooms = json.load(file)
+
+    async with engine_null_pool.begin() as conn:
+        await conn.execute(HotelsModel.__table__.insert(), hotels)
+        await conn.execute(RoomsModel.__table__.insert(), rooms)
+        await conn.execute(text("SELECT setval('hotels_id_seq', (SELECT MAX(id) FROM hotels))"))
+        await conn.execute(text("SELECT setval('rooms_id_seq', (SELECT MAX(id) FROM rooms))"))
